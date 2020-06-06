acd9e36d10a391105279e8078a4941e8ec8048f1
KacperGawdzinski <58347130+KacperGawdzinski@users.noreply.github.com>
2020-06-06 17:16:15 +0200

    added new chart at homepage

--- WPF Budget Project/Home.xaml.cs
+++ WPF Budget Project/Home.xaml.cs
@@ -35,9 +35,9 @@ namespace WPF_Budget_Project
             command.ExecuteNonQuery();
             m_dbConnection.Close();*/
 
-            SeriesCollection = new SeriesCollection
+            Rounded = new SeriesCollection
             {
-                /*new PieSeries
+              /*  new PieSeries
                 {
                     Title = "Food",
                     Values = new ChartValues<ObservableValue> { new ObservableValue(20) },
@@ -63,14 +63,58 @@ namespace WPF_Budget_Project
                 }*/
             };
             DataContext = this;
+
+            Basic = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                },
+                /*new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }*/
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            /*Basic.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+                PointGeometrySize = 50,
+                PointForeground = Brushes.Gray
+            });
+
+            //modifying any series values will also animate and update the chart
+            Basic[3].Values.Add(5d);*/
+
+            DataContext = this;
         }
 
-        public SeriesCollection SeriesCollection { get; set; }
+        public SeriesCollection Rounded { get; set; }
+        public SeriesCollection Basic { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
 
         private void AddSeriesOnClick(object sender, RoutedEventArgs e)
         {
             var r = new Random();
-            var c = SeriesCollection.Count > 0 ? SeriesCollection[0].Values.Count : 1;
+            var c = Rounded.Count > 0 ? Rounded[0].Values.Count : 1;
 
             var vals = new ChartValues<ObservableValue>();
 
@@ -79,7 +123,7 @@ namespace WPF_Budget_Project
                 vals.Add(new ObservableValue(r.Next(0, 10)));
             }
 
-            SeriesCollection.Add(new PieSeries
+            Rounded.Add(new PieSeries
             {
                 Values = vals
             });
@@ -87,8 +131,8 @@ namespace WPF_Budget_Project
 
         private void RemoveSeriesOnClick(object sender, RoutedEventArgs e)
         {
-            if (SeriesCollection.Count > 0)
-                SeriesCollection.RemoveAt(0);
+            if (Rounded.Count > 0)
+                Rounded.RemoveAt(0);
         }
 
         private void RestartOnClick(object sender, RoutedEventArgs e)
